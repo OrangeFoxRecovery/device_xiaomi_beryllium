a5825fc9c18ffa53dc95fd2c4e2c158e5ae30896
Sushrut1101 <guptasushrut@gmail.com>
2022-03-24 11:00:39 +0530

    beryllium: Cleanup vendorsetup.sh
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,19 @@
+#
+# Copyright (C) 2022 The OrangeFox Recovery Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 FDEVICE="beryllium"
 
 fox_get_target_device() {
@@ -33,7 +49,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	export OF_USE_NEW_MAGISKBOOT=1
 	export OF_SKIP_MULTIUSER_FOLDERS_BACKUP=1
 	export FOX_DELETE_AROMAFM=1
-    export FOX_ENABLE_APP_MANAGER=1
+	export FOX_ENABLE_APP_MANAGER=1
 
 	# Screen Settings
 	export ALLOW_DISABLE_NAVBAR=0
