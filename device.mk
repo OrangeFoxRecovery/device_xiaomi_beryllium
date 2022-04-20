af1d60e3d690946a4120186e745ff2497cb3685a
Sushrut1101 <guptasushrut@gmail.com>
2022-04-20 18:54:06 +0530

    beryllium: Update Blobs
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- device.mk
+++ device.mk
@@ -21,25 +21,23 @@ PRODUCT_PACKAGES += \
 
 # Additional Libraries
 TARGET_RECOVERY_DEVICE_MODULES += \
-	libicuuc \
-	libandroidicu \
 	libion \
 	libxml2 \
-	libcap \
+	libandroidicu \
+	libicuuc \
 	libicui18n \
-	vendor.display.config@1.0 \
-	android.system.suspend@1.0 \
-	libpcrecpp
+	libqcbor \
+	libhardware_legacy \
+	android.system.suspend@1.0
 
 TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
-	$(TARGET_OUT_SHARED_LIBRARIES)/libicuuc.so \
 	$(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
 	$(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so \
-	$(TARGET_OUT_SHARED_LIBRARIES)/libcap.so \
+	$(TARGET_OUT_SHARED_LIBRARIES)/libicuuc.so \
 	$(TARGET_OUT_SHARED_LIBRARIES)/libicui18n.so \
-	$(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@1.0.so \
-	$(TARGET_OUT_SHARED_LIBRARIES)/android.system.suspend@1.0.so \
-	$(TARGET_OUT_SHARED_LIBRARIES)/libpcrecpp.so
+	$(TARGET_OUT_SHARED_LIBRARIES)/libqcbor.so \
+	$(TARGET_OUT_SHARED_LIBRARIES)/libhardware_legacy.so \
+	$(TARGET_OUT_SHARED_LIBRARIES)/android.system.suspend@1.0.so
 
 PRODUCT_COPY_FILES += \
-	$(OUT_DIR)/target/product/$(PRODUCT_DEVICE)/obj/SHARED_LIBRARIES/libandroidicu_intermediates/libandroidicu.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib64/libandroidicu.so
+	$(OUT_DIR)/target/product/beryllium/obj/SHARED_LIBRARIES/libandroidicu_intermediates/libandroidicu.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib64/libandroidicu.so
